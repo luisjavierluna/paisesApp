d4d35d6ba5d1785ba02f62cc111988087e713cc6
luisjavierluna <javierlunaluis@gmail.com>
2023-06-16 20:35:23 -0600

    18. DebounceTime - en el input

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 
 @Component({
@@ -7,14 +8,28 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styles: [
   ]
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit {
+  @Output() onEnter  : EventEmitter<string> = new EventEmitter();
+  @Output() onDebouce: EventEmitter<string> = new EventEmitter();
 
-  @Output() onEnter: EventEmitter<string> = new EventEmitter();
-
+  debouncer: Subject<string> = new Subject()
+  
   termino:string = ''
+
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(debounceTime(300)) 
+      .subscribe( valor => {
+        this.onDebouce.emit(valor)
+      })
+  }
 
   buscar() {
     this.onEnter.emit(this.termino)
+  }
+
+  teclaPresionada() {
+    this.debouncer.next(this.termino)
   }
 
 }
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -34,4 +34,8 @@ export class PorPaisComponent {
       })
   }
 
+  sugerencias(termino: string) {
+    this.hayError = false
+  }
+
 }
