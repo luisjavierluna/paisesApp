26829bb06cfda7ef928a5f72204cf3157e22a367
luisjavierluna <javierlunaluis@gmail.com>
2023-06-18 18:56:48 -0600

    4. ngClass, class y [class.]

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+  button {
+    margin-right: 5px;
+  }
+  `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europa', 'oceania']
+  regionActiva: string = ''
 
   constructor() { }
 
-  ngOnInit(): void {
+  activarRegion( region: string) {
+    this.regionActiva = region
   }
 
 }
