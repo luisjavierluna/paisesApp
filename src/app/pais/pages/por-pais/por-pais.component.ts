e806db1b683ef2a0869625e540303434db032520
luisjavierluna <javierlunaluis@gmail.com>
2023-06-19 18:41:30 -0600

    9. Mostrar sugerencias al escribir - autocomplete

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
 import { Country } from '../../interfaces/pais-interface';
+import { error } from 'console';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li{
+        cursor:pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -13,6 +19,9 @@ export class PorPaisComponent {
   termino: string = ''
   hayError: boolean = false
   paises: Country[] = []
+
+  paisesSugeridos: Country[] = []
+  mostrarSugerencias: boolean = false
 
   constructor(private paisService: PaisService) { }
 
@@ -36,6 +45,18 @@ export class PorPaisComponent {
 
   sugerencias(termino: string) {
     this.hayError = false
+    this.termino = termino
+    this.mostrarSugerencias = true
+
+    this.paisService.buscarPais(termino)
+      .subscribe({
+        next: paises => this.paisesSugeridos = paises.splice(0, 5),
+        error: err => this.paisesSugeridos = []
+      })
+  }
+
+  buscarSugerido( termino : string) {
+    this.buscar(termino)
   }
 
 }
